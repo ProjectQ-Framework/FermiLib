2e3a528d1b6c0f2ff9f90cb1690da6da2e4a85f3
Craig Gidney <craiggidney@google.com>
2017-05-18 17:53:58 -0700

    Extract methods from bravyi_kitaev - Moved FenwickTree building out of the loop. - Extracted inline_product and inline_sum (likely useful elsewhere) - Extracted _transform_ladder_operator and _transform_operator_term - Using generators to build values

--- src/fermilib/transforms/_bravyi_kitaev.py
+++ src/fermilib/transforms/_bravyi_kitaev.py
@@ -22,7 +22,11 @@ def bravyi_kitaev(operator, n_qubits=None):
     """Apply the Bravyi-Kitaev transform and return qubit operator.
 
     Args:
-        operator: A FermionOperator to transform.
+        operator (fermilib.ops.FermionOperator):
+            A FermionOperator to transform.
+        n_qubits (int|None):
+            Can force the number of qubits in the resulting operator above the
+            number that appear in the input operator.
 
     Returns:
         transformed_operator: An instance of the QubitOperator class.
@@ -37,53 +41,104 @@ def bravyi_kitaev(operator, n_qubits=None):
     if n_qubits < count_qubits(operator):
         raise ValueError('Invalid number of qubits specified.')
 
+    # Build the Fenwick tree.
+    fenwick_tree = FenwickTree(n_qubits)
+
     # Compute transformed operator.
-    transformed_operator = QubitOperator()
-    for term in operator.terms:
+    transformed_terms = (
+        _transform_operator_term(term=term,
+                                 coefficient=operator.terms[term],
+                                 fenwick_tree=fenwick_tree)
+        for term in operator.terms
+    )
+    return inline_sum(seed=QubitOperator(), summands=transformed_terms)
 
-        # Initialize identity matrix.
-        coefficient = operator.terms[term]
-        transformed_term = QubitOperator((), coefficient)
 
-        # Build the Fenwick Tree
-        fenwick_tree = FenwickTree(n_qubits)
+def _transform_operator_term(term, coefficient, fenwick_tree):
+    """
+    Args:
+        term (list[tuple[int, int]]):
+            A list of (mode, raising-vs-lowering) ladder operator terms.
+        coefficient (float):
+        fenwick_tree (FenwickTree):
+    Returns:
+        QubitOperator:
+    """
 
-        # Build the Bravyi-Kitaev transformed operators.
-        for ladder_operator in term:
-            index = ladder_operator[0]
+    # Build the Bravyi-Kitaev transformed operators.
+    transformed_ladder_ops = (
+        _transform_ladder_operator(ladder_operator, fenwick_tree)
+        for ladder_operator in term
+    )
+    return inline_product(seed=QubitOperator((), coefficient),
+                          factors=transformed_ladder_ops)
 
-            # Parity set. Set of nodes to apply Z to.
-            parity_set = [node.index for node in
-                          fenwick_tree.get_parity_set(index)]
 
-            # Update set. Set of ancestors to apply X to.
-            ancestors = [node.index for node in
-                         fenwick_tree.get_update_set(index)]
+def _transform_ladder_operator(ladder_operator, fenwick_tree):
+    """
+    Args:
+        ladder_operator (tuple[int, int]):
+        fenwick_tree (FenwickTree):
+    Returns:
+        QubitOperator:
+    """
+    index = ladder_operator[0]
 
-            # The C(j) set.
-            ancestor_children = [node.index for node in
-                                 fenwick_tree.get_remainder_set(index)]
+    # Parity set. Set of nodes to apply Z to.
+    parity_set = [node.index for node in
+                  fenwick_tree.get_parity_set(index)]
 
-            # Switch between lowering/raising operators.
-            d_coefficient = .5j
-            if ladder_operator[1]:
-                d_coefficient *= -1.
+    # Update set. Set of ancestors to apply X to.
+    ancestors = [node.index for node in
+                 fenwick_tree.get_update_set(index)]
 
-            # The fermion lowering operator is given by
-            # a = (c+id)/2 where c, d are the majoranas.
-            d_majorana_component = QubitOperator(
-                (((ladder_operator[0], 'Y'),) +
-                 tuple((index, 'Z') for index in ancestor_children) +
-                 tuple((index, 'X') for index in ancestors)),
-                d_coefficient)
+    # The C(j) set.
+    ancestor_children = [node.index for node in
+                         fenwick_tree.get_remainder_set(index)]
 
-            c_majorana_component = QubitOperator(
-                (((ladder_operator[0], 'X'),) +
-                 tuple((index, 'Z') for index in parity_set) +
-                 tuple((index, 'X') for index in ancestors)),
-                0.5)
+    # Switch between lowering/raising operators.
+    d_coefficient = .5j
+    if ladder_operator[1]:
+        d_coefficient *= -1.
 
-            # Update term.
-            transformed_term *= c_majorana_component + d_majorana_component
-        transformed_operator += transformed_term
-    return transformed_operator
+    # The fermion lowering operator is given by
+    # a = (c+id)/2 where c, d are the majoranas.
+    d_majorana_component = QubitOperator(
+        (((ladder_operator[0], 'Y'),) +
+         tuple((index, 'Z') for index in ancestor_children) +
+         tuple((index, 'X') for index in ancestors)),
+        d_coefficient)
+
+    c_majorana_component = QubitOperator(
+        (((ladder_operator[0], 'X'),) +
+         tuple((index, 'Z') for index in parity_set) +
+         tuple((index, 'X') for index in ancestors)),
+        0.5)
+
+    return c_majorana_component + d_majorana_component
+
+
+def inline_sum(seed, summands):
+    """Computes a product, using the __iadd__ operator.
+    Args:
+        seed (T): The starting total. The zero value.
+        summands (iterable[T]): Values to add (with +=) into the total.
+    Returns:
+        T: The result of adding all the factors into the zero value.
+    """
+    for r in summands:
+        seed += r
+    return seed
+
+
+def inline_product(seed, factors):
+    """Computes a product, using the __imul__ operator.
+    Args:
+        seed (T): The starting total. The unit value.
+        factors (iterable[T]): Values to multiply (with *=) into the total.
+    Returns:
+        T: The result of multiplying all the factors into the unit value.
+    """
+    for r in factors:
+        seed *= r
+    return seed
