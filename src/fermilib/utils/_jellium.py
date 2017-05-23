5df2301de1ef3fc14b3f2098851ae587898926fb
Craig Gidney <craiggidney@google.com>
2017-05-23 11:43:04 -0700

    Polish pydoc in _jellium.py

--- src/fermilib/utils/_jellium.py
+++ src/fermilib/utils/_jellium.py
@@ -29,14 +29,16 @@ def orbital_id(grid_length, grid_coordinates, spin=None):
 
     Args:
         grid_length: Int, the number of points in one dimension of the grid.
-        grid_coordinates: List or tuple of ints giving coordinates of grid
-            element. Acceptable to provide an int (instead of tuple or list)
-            for 1D case.
-        spin: Boole, 0 means spin down and 1 means spin up.
+        grid_coordinates (int|iterable[int]):
+            List or tuple of ints giving coordinates of grid element.
+            Acceptable to provide an int (instead of tuple or list) for 1D
+            case.
+        spin (0|1|None): 0 means spin down and 1 means spin up.
             If None, assume spinless model.
 
     Returns:
-        tensor_factor: tensor factor associated with provided orbital label.
+        tensor_factor (int):
+            tensor factor associated with provided orbital label.
     """
     # Initialize.
     if isinstance(grid_coordinates, int):
@@ -68,12 +70,13 @@ def grid_indices(qubit_id, grid, spinless):
     """This function is the inverse of orbital_id.
 
     Args:
-        qubit_id: The tensor factor to map to grid indices.
+        qubit_id (int): The tensor factor to map to grid indices.
         grid (Grid): The discretization to use.
         spinless (bool): Whether to use the spinless model or not.
 
     Returns:
-        grid_indices: The location of the qubit on the grid.
+        grid_indices (numpy.ndarray[int]):
+            The location of the qubit on the grid.
     """
     # Remove spin degree of freedom.
     orbital_id = qubit_id
@@ -95,8 +98,9 @@ def position_vector(position_indices, grid):
     """Given grid point coordinate, return position vector with dimensions.
 
     Args:
-        position_indices: List or tuple of integers giving grid point
-            coordinate. Allowed values are ints in [0, grid_length).
+        position_indices (int|iterable[int]):
+            List or tuple of integers giving grid point coordinate.
+            Allowed values are ints in [0, grid_length).
         grid (Grid): The discretization to use.
 
     Returns:
@@ -111,8 +115,7 @@ def position_vector(position_indices, grid):
 
     # Compute position vector.
     adjusted_vector = numpy.array(position_indices, float) - grid.length // 2
-    position_vector = grid.scale * adjusted_vector / float(grid.length)
-    return position_vector
+    return grid.scale * adjusted_vector / float(grid.length)
 
 
 def momentum_vector(momentum_indices, grid):
@@ -136,8 +139,7 @@ def momentum_vector(momentum_indices, grid):
 
     # Compute momentum vector.
     adjusted_vector = numpy.array(momentum_indices, float) - grid.length // 2
-    momentum_vector = 2. * numpy.pi * adjusted_vector / grid.scale
-    return momentum_vector
+    return 2. * numpy.pi * adjusted_vector / grid.scale
 
 
 def momentum_kinetic_operator(grid, spinless=False):
@@ -178,19 +180,16 @@ def momentum_potential_operator(grid, spinless=False):
 
     Args:
         grid (Grid): The discretization to use.
-        spinless: Boole, whether to use the spinless model or not.
+        spinless (bool): Whether to use the spinless model or not.
 
     Returns:
-        operator: An instance of the FermionOperator class.
+        operator (FermionOperator)
     """
     # Initialize.
     volume = grid.volume_scale()
     prefactor = 2. * numpy.pi / volume
     operator = FermionOperator((), 0.0)
-    if spinless:
-        spins = [None]
-    else:
-        spins = [0, 1]
+    spins = [None] if spinless else [0, 1]
 
     # Loop once through all plane waves.
     for omega_indices in grid.all_points_indices():
@@ -205,8 +204,7 @@ def momentum_potential_operator(grid, spinless=False):
             continue
 
         # Compute coefficient.
-        coefficient = prefactor / \
-            omega_momenta.dot(omega_momenta)
+        coefficient = prefactor / omega_momenta.dot(omega_momenta)
 
         for grid_indices_a in grid.all_points_indices():
             shifted_indices_d = [
@@ -246,18 +244,15 @@ def position_kinetic_operator(grid, spinless=False):
 
     Args:
         grid (Grid): The discretization to use.
-        spinless: Bool, whether to use the spinless model or not.
+        spinless (bool): Whether to use the spinless model or not.
 
     Returns:
-        operator: An instance of the FermionOperator class.
+        operator (FermionOperator)
     """
     # Initialize.
     n_points = grid.num_points()
     operator = FermionOperator()
-    if spinless:
-        spins = [None]
-    else:
-        spins = [0, 1]
+    spins = [None] if spinless else [0, 1]
 
     # Loop once through all lattice sites.
     for grid_indices_a in grid.all_points_indices():
@@ -293,19 +288,16 @@ def position_potential_operator(grid, spinless=False):
 
     Args:
         grid (Grid): The discretization to use.
-        spinless: Boole, whether to use the spinless model or not.
+        spinless (bool): Whether to use the spinless model or not.
 
     Returns:
-        operator: An instance of the FermionOperator class.
+        operator (FermionOperator)
     """
     # Initialize.
     volume = grid.volume_scale()
     prefactor = 2. * numpy.pi / volume
     operator = FermionOperator()
-    if spinless:
-        spins = [None]
-    else:
-        spins = [0, 1]
+    spins = [None] if spinless else [0, 1]
 
     # Loop once through all lattice sites.
     for grid_indices_a in grid.all_points_indices():
@@ -368,10 +360,10 @@ def jordan_wigner_position_jellium(grid, spinless=False):
 
     Args:
         grid (Grid): The discretization to use.
-        spinless: Bool, whether to use the spinless model or not.
+        spinless (bool): Whether to use the spinless model or not.
 
     Returns:
-        hamiltonian: An instance of the QubitOperator class.
+        hamiltonian (QubitOperator)
     """
     # Initialize.
     n_orbitals = grid.num_points()
