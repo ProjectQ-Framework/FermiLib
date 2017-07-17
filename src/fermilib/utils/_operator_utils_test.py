e1aac772320a293125235845ee71ea4f4b851aec
idk3 <kivlichan@g.harvard.edu>
2017-07-16 17:55:52 -0700

    Added functions for jellium Trotter error (#111)
    * Added functions for jellium Trotter error
    
    * Ensuring proper dict.values() type
    
    * Actually updating instead of uploading randomly
    
    * Deleting misplaced file
    
    * Updates addressing PR comments
    
    Significant change: all functions can now handle full dual basis Hamiltonian. When it makes a difference an optional parameter jellium_only can be specified.
    
    The only change outside the _dual_basis_trotter files are 1. changing the commutator in operator_utils and 2. moving commutator tests to _operator_utils_test.
    
    * Adding things in the right place for once
    
    * Adding things in the right place for once v2
    
    * Removed __name__ = __main__ block in dual_basis_trotter_error
    
    * Fixed naming and indentation causing test coverage drop
    
    * Function name change in test file
    
    * Restoring n_qubits variable which got killed in indentation earlier
    
    * More name changes to fix internal error

--- src/fermilib/utils/_operator_utils_test.py
+++ src/fermilib/utils/_operator_utils_test.py
@@ -20,6 +20,8 @@ import unittest
 from fermilib.config import *
 from fermilib.ops import *
 from fermilib.transforms import jordan_wigner, get_interaction_operator
+from fermilib.utils import (eigenspectrum, commutator,
+                            count_qubits, is_identity)
 from fermilib.utils._operator_utils import (commutator, count_qubits,
                                             eigenspectrum, get_file_path,
                                             is_identity, load_operator,
@@ -39,13 +41,19 @@ class OperatorUtilsTest(unittest.TestCase):
         self.interaction_operator = get_interaction_operator(
             self.fermion_operator)
 
-    def test_n_qubits(self):
+    def test_n_qubits_single_fermion_term(self):
         self.assertEqual(self.n_qubits,
                          count_qubits(self.fermion_term))
+
+    def test_n_qubits_fermion_operator(self):
         self.assertEqual(self.n_qubits,
                          count_qubits(self.fermion_operator))
+
+    def test_n_qubits_qubit_operator(self):
         self.assertEqual(self.n_qubits,
                          count_qubits(self.qubit_operator))
+
+    def test_n_qubits_interaction_operator(self):
         self.assertEqual(self.n_qubits,
                          count_qubits(self.interaction_operator))
 
@@ -59,32 +67,38 @@ class OperatorUtilsTest(unittest.TestCase):
             self.assertAlmostEqual(fermion_eigenspectrum[i],
                                    interaction_eigenspectrum[i])
 
-    def test_is_identity(self):
+    def test_is_identity_unit_fermionoperator(self):
         self.assertTrue(is_identity(FermionOperator(())))
+
+    def test_is_identity_double_of_unit_fermionoperator(self):
         self.assertTrue(is_identity(2. * FermionOperator(())))
+
+    def test_is_identity_unit_qubitoperator(self):
         self.assertTrue(is_identity(QubitOperator(())))
+
+    def test_is_identity_double_of_unit_qubitoperator(self):
         self.assertTrue(is_identity(QubitOperator((), 2.)))
+
+    def test_not_is_identity_single_term_fermionoperator(self):
         self.assertFalse(is_identity(FermionOperator('1^')))
+
+    def test_not_is_identity_single_term_qubitoperator(self):
         self.assertFalse(is_identity(QubitOperator('X1')))
+
+    def test_not_is_identity_zero_fermionoperator(self):
         self.assertFalse(is_identity(FermionOperator()))
+
+    def test_not_is_identity_zero_qubitoperator(self):
         self.assertFalse(is_identity(QubitOperator()))
 
-    def test_commutator(self):
-        operator_a = FermionOperator('')
-        self.assertTrue(FermionOperator().isclose(
-            commutator(operator_a, self.fermion_operator)))
-        operator_b = QubitOperator('X1 Y2')
-        self.assertTrue(commutator(self.qubit_operator, operator_b).isclose(
-            self.qubit_operator * operator_b -
-            operator_b * self.qubit_operator))
 
-    def test_commutator_operator_b_bad_type_raise_TypeError(self):
-        with self.assertRaises(TypeError):
-            commutator(1, self.fermion_operator)
-
-    def test_commutator_operator_b_bad_type_raise_TypeError(self):
-        with self.assertRaises(TypeError):
-            commutator(self.qubit_operator, "hello")
+class SaveLoadOperatorTest(unittest.TestCase):
+    def setUp(self):
+        self.n_qubits = 5
+        self.fermion_term = FermionOperator('1^ 2^ 3 4', -3.17)
+        self.fermion_operator = self.fermion_term + hermitian_conjugated(
+            self.fermion_term)
+        self.qubit_operator = jordan_wigner(self.fermion_operator)
 
     def test_save_and_load_operators(self):
         file_name = "test_file"
@@ -124,6 +138,71 @@ class OperatorUtilsTest(unittest.TestCase):
             constant, one_body, two_body)
         with self.assertRaises(TypeError):
             save_operator(interaction_operator, file_name)
+
+
+class CommutatorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.fermion_term = FermionOperator('1^ 2^ 3 4', -3.17)
+        self.fermion_operator = self.fermion_term + hermitian_conjugated(
+            self.fermion_term)
+        self.qubit_operator = jordan_wigner(self.fermion_operator)
+
+    def test_commutes_identity(self):
+        com = commutator(FermionOperator.identity(),
+                         FermionOperator('2^ 3', 2.3))
+        self.assertTrue(com.isclose(FermionOperator.zero()))
+
+    def test_commutes_no_intersection(self):
+        com = commutator(FermionOperator('2^ 3'), FermionOperator('4^ 5^ 3'))
+        com = normal_ordered(com)
+        self.assertTrue(com.isclose(FermionOperator.zero()))
+
+    def test_commutes_number_operators(self):
+        com = commutator(FermionOperator('4^ 3^ 4 3'), FermionOperator('2^ 2'))
+        com = normal_ordered(com)
+        self.assertTrue(com.isclose(FermionOperator.zero()))
+
+    def test_commutator_hopping_operators(self):
+        com = commutator(3 * FermionOperator('1^ 2'), FermionOperator('2^ 3'))
+        com = normal_ordered(com)
+        self.assertTrue(com.isclose(FermionOperator('1^ 3', 3)))
+
+    def test_commutator_hopping_with_single_number(self):
+        com = commutator(FermionOperator('1^ 2', 1j), FermionOperator('1^ 1'))
+        com = normal_ordered(com)
+        self.assertTrue(com.isclose(-FermionOperator('1^ 2') * 1j))
+
+    def test_commutator_hopping_with_double_number_one_intersection(self):
+        com = commutator(FermionOperator('1^ 3'), FermionOperator('3^ 2^ 3 2'))
+        com = normal_ordered(com)
+        self.assertTrue(com.isclose(-FermionOperator('2^ 1^ 3 2')))
+
+    def test_commutator_hopping_with_double_number_two_intersections(self):
+        com = commutator(FermionOperator('2^ 3'), FermionOperator('3^ 2^ 3 2'))
+        com = normal_ordered(com)
+        self.assertTrue(com.isclose(FermionOperator.zero()))
+
+    def test_commutator(self):
+        operator_a = FermionOperator('')
+        self.assertTrue(FermionOperator().isclose(
+            commutator(operator_a, self.fermion_operator)))
+        operator_b = QubitOperator('X1 Y2')
+        self.assertTrue(commutator(self.qubit_operator, operator_b).isclose(
+            self.qubit_operator * operator_b -
+            operator_b * self.qubit_operator))
+
+    def test_commutator_operator_b_bad_type_raise_TypeError(self):
+        with self.assertRaises(TypeError):
+            commutator(1, self.fermion_operator)
+
+    def test_commutator_operator_b_bad_type_raise_TypeError(self):
+        with self.assertRaises(TypeError):
+            commutator(self.qubit_operator, "hello")
+
+    def test_commutator_not_same_type(self):
+        with self.assertRaises(TypeError):
+            commutator(self.fermion_operator, self.qubit_operator)
 
 
 if __name__ == '__main__':
