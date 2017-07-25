d0d4bdf8fd314d45e017cceca958e02f66f00a54
idk3 <kivlichan@g.harvard.edu>
2017-07-25 15:34:15 -0700

    Renamings and other cleanup

--- src/fermilib/utils/_parallel_bubble_sort.py
+++ src/fermilib/utils/_parallel_bubble_sort.py
@@ -10,43 +10,52 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-"""This module has functions to perform parallel bubble sort on arrays."""
+"""This module has functions to perform parallel bubble sort on arrays.
+
+Within the context of FermiLib, parallel bubble sort can be used to
+quickly reorder fermionic modes or qubits.
+"""
 from functools import partial
 from itertools import product
 
 
-def index(coordinate_ordering, system_side_length, position):
-    """Return the index of an n-dimensional position in an 1-dimensional
-    array.
+def index_of_position_in_1d_array(coordinate_ordering, system_side_length,
+                                  position):
+    """Return the index of an n-dimensional position embedded in a
+    1-dimensional array.
 
     The index is computed using the given ordering of coordinate
     weights as well as the side length of the system from which the
     n-dimensional position is taken.
 
     Args:
-        position: The vector (list) of integers indicating the position
-                  within the n-dimensional system.
-        system_side_length: The number of points along a side of the system.
-        coordinate_ordering: Tuple indicating the order of coordinates
-                             by which to increment the index.
+        position (list of ints): The vector indicating the position
+                within the n-dimensional system.
+        system_side_length (int): The number of points along a side of
+                the system.
+        coordinate_ordering (list of ints): The order of coordinates by
+                which to increment the index.
 
     Returns:
         The integer index of the position in a 1D array.
 
     Examples:
-        index((2, 1, 0), 4, (1, 2, 3)) -> 27 (16 + 4 * 2 + 1 * 3).
-        This is standard lexicographic ordering for a 3D array.
+        index_of_position_in_1d_array((2, 1, 0), 4, (1, 2, 3))
+                -> 27 (16 + 4 * 2 + 1 * 3).
+        This is standard lexicographic ordering for 3D positions.
 
-        index((0, 1, 2), 2, (0, 1, 1)) -> 6 (2 * 1 + 4 * 1)
+        index_of_position_in_1d_array((0, 1, 2), 2, (0, 1, 1))
+                -> 6 (2 * 1 + 4 * 1)
 
-        index((2, 0, 1), 3, (2, 1, 0)) -> 19 (9 * 2 + 1 + 3 * 0)
+        index_of_position_in_1d_array((2, 0, 1), 3, (2, 1, 0))
+                -> 19 (9 * 2 + 1 + 3 * 0)
     """
     return sum(position[i] * system_side_length ** coordinate_ordering[i]
                for i in range(len(coordinate_ordering)))
 
 
-def is_sorted_embedded_array(arr, key, system_side_length):
-    """Determine whether an embedded n-dim array of positions is sorted.
+def is_sorted_array_of_nd_positions(arr, key, system_side_length):
+    """Determine whether an array of n-dim positions is sorted.
 
     Args:
         arr: the array to work with.
@@ -55,6 +64,9 @@ def is_sorted_embedded_array(arr, key, system_side_length):
                             cube from which the positions in the array
                             are taken.
     """
+    if isinstance(key, tuple):
+        key = partial(*key)
+
     for i in range(len(arr) - 1):
         if (key(system_side_length, arr[i]) >
                 key(system_side_length, arr[i + 1])):
@@ -76,7 +88,10 @@ def parallel_bubble_sort(array, key, system_side_length):
     swaps = []
     odd = 0
 
-    while not is_sorted_embedded_array(array, key, system_side_length):
+    if isinstance(key, tuple):
+        key = partial(*key)
+
+    while not is_sorted_array_of_nd_positions(array, key, system_side_length):
         swaps.append(parallel_bubble_sort_single_step(
             array, key, odd, system_side_length=system_side_length))
         odd = 1 - odd
@@ -104,6 +119,9 @@ def parallel_bubble_sort_single_step(array, key, odd=False,
 
     if system_side_length is None:
         system_side_length = len(array)
+
+    if isinstance(key, tuple):
+        key = partial(*key)
 
     for i in range(int(odd), len(array) - 1, 2):
         if (key(system_side_length, array[i]) >
--- src/fermilib/utils/_parallel_bubble_sort_test.py
+++ src/fermilib/utils/_parallel_bubble_sort_test.py
@@ -13,77 +13,85 @@
 """Tests for _parallel_bubble_sort.py."""
 import unittest
 
-from functools import partial
 from fermilib.utils._parallel_bubble_sort import (
-    index, is_sorted_embedded_array, parallel_bubble_sort)
+    index_of_position_in_1d_array, is_sorted_array_of_nd_positions,
+    parallel_bubble_sort)
 
 
 class IndexTest(unittest.TestCase):
     def test_index_standard_order_coordinates(self):
-        self.assertEqual(index((2, 1, 0), 3, (1, 2, 3)),
-                         (3 ** 2) * 1 + (3 ** 1) * 2 + (3 ** 0) * 3)
+        self.assertEqual(
+            index_of_position_in_1d_array(
+                (2, 1, 0), 3, (1, 2, 3)),
+            ((3 ** 2) * 1 + (3 ** 1) * 2 + (3 ** 0) * 3))
 
     def test_index_reverse_order_coordinates(self):
-        self.assertEqual(index((0, 1, 2), 3, (2, 3, 5)),
-                         (3 ** 0) * 2 + (3 ** 1) * 3 + (3 ** 2) * 5)
+        self.assertEqual(
+            index_of_position_in_1d_array(
+                (0, 1, 2), 3, (2, 3, 5)),
+            ((3 ** 0) * 2 + (3 ** 1) * 3 + (3 ** 2) * 5))
 
     def test_index_scrambled_coordinates(self):
-        self.assertEqual(index((2, 0, 1), 4, (2, 1, 0)),
-                         (4 ** 2) * 2 + (4 ** 0) * 1 + (4 ** 1) * 0)
+        self.assertEqual(
+            index_of_position_in_1d_array(
+                (2, 0, 1), 4, (2, 1, 0)),
+            ((4 ** 2) * 2 + (4 ** 0) * 1 + (4 ** 1) * 0))
 
     def test_index_scrambled_coordinates_6dimensions(self):
-        self.assertEqual(index((1, 2, 0, 5, 4, 3), 3, (2, 1, 0, 0, 1, 2)),
-                         (3 ** 1) * 2 + (3 ** 2) + (3 ** 4) + (3 ** 3) * 2)
+        self.assertEqual(
+            index_of_position_in_1d_array(
+                (1, 2, 0, 5, 4, 3), 3, (2, 1, 0, 0, 1, 2)),
+            (3 ** 1) * 2 + (3 ** 2) + (3 ** 4) + (3 ** 3) * 2)
 
 
 class IsSortedTest(unittest.TestCase):
     def test_is_sorted_standard_order(self):
-        key = partial(index, (2, 1, 0))
+        key = (index_of_position_in_1d_array, (2, 1, 0))
         arr = [(0, 0, i) for i in range(7)]
-        self.assertTrue(is_sorted_embedded_array(arr, key, 7))
+        self.assertTrue(is_sorted_array_of_nd_positions(arr, key, 7))
 
     def test_not_is_sorted_reverse_ordered_list(self):
-        key = partial(index, (2, 1, 0))
+        key = (index_of_position_in_1d_array, (2, 1, 0))
         arr = [(0, 0, i) for i in range(7, -1, -1)]
-        self.assertFalse(is_sorted_embedded_array(arr, key, 7))
+        self.assertFalse(is_sorted_array_of_nd_positions(arr, key, 7))
 
     def test_is_sorted_3d(self):
-        key = partial(index, (2, 1, 0))
+        key = (index_of_position_in_1d_array, (2, 1, 0))
         arr = [(i, j, k) for i in range(8)
                for j in range(8)
                for k in range(8)]
-        self.assertTrue(is_sorted_embedded_array(arr, key, 8))
+        self.assertTrue(is_sorted_array_of_nd_positions(arr, key, 8))
 
     def test_not_is_sorted_2d_last_elements_swapped(self):
-        key = partial(index, (1, 0))
+        key = (index_of_position_in_1d_array, (1, 0))
         arr = [(i, j) for i in range(12) for j in range(12)]
         arr[-1], arr[-2] = arr[-2], arr[-1]
-        self.assertFalse(is_sorted_embedded_array(arr, key, 12))
+        self.assertFalse(is_sorted_array_of_nd_positions(arr, key, 12))
 
 
 class ParallelBubbleSort1DTest(unittest.TestCase):
     def test_sort_4_elements(self):
-        key = partial(index, (2, 1, 0))
+        key = (index_of_position_in_1d_array, (2, 1, 0))
         arr = [(1, 0, 0), (0, 0, 1), (0, 1, 0), (0, 0, 0)]
         parallel_bubble_sort(arr, key, system_side_length=2)
         self.assertListEqual(arr,
                              [(0, 0, 0), (0, 0, 1), (0, 1, 0), (1, 0, 0)])
 
     def test_sort_reverse_long_no_optional_parameters_specified(self):
-        key = partial(index, (2, 0, 1))
+        key = (index_of_position_in_1d_array, (2, 0, 1))
         arr = [(0, i, 0) for i in range(10, -1, -1)]
         parallel_bubble_sort(arr, key, system_side_length=11)
         self.assertListEqual(arr, [(0, i, 0) for i in range(11)])
 
     def test_sort_scrambled(self):
-        key = partial(index, (0, 1))
+        key = (index_of_position_in_1d_array, (0, 1))
         arr = [(1, 2), (0, 1), (2, 1), (1, 0), (0, 0), (2, 0)]
         parallel_bubble_sort(arr, key, system_side_length=3)
         self.assertListEqual(arr, [(0, 0), (1, 0), (2, 0),
                                    (0, 1), (2, 1), (1, 2)])
 
     def test_sort_already_sorted(self):
-        key = partial(index, (2, 1, 0))
+        key = (index_of_position_in_1d_array, (2, 1, 0))
         arr = [(0, i, j) for i in range(5) for j in range(5)]
         parallel_bubble_sort(arr, key, system_side_length=5)
         self.assertListEqual(arr, [(0, i, j) for i in range(5)
@@ -92,7 +100,9 @@ class ParallelBubbleSort1DTest(unittest.TestCase):
 
 class ParallelBubbleSortIntegrationTest(unittest.TestCase):
     def test_sort_2d_grid_embedded_in_1d_array_integration_test(self):
-        key_2d = partial(index, (0, 1))
+        from functools import partial
+
+        key_2d = partial(index_of_position_in_1d_array, (0, 1))
         system_size = 8
         arr = [(i, j) for i in range(system_size)
                for j in range(system_size)]
@@ -100,10 +110,12 @@ class ParallelBubbleSortIntegrationTest(unittest.TestCase):
         self.assertListEqual(list(map(partial(key_2d, system_size), arr))[:16],
                              range(0, 64, 8) + range(1, 65, 8))
 
-        self.assertFalse(is_sorted_embedded_array(arr, key_2d, system_size))
+        self.assertFalse(is_sorted_array_of_nd_positions(arr, key_2d,
+                                                         system_size))
         swaps = parallel_bubble_sort(arr, key_2d, system_size)
         self.assertEqual(len(swaps), 50)
-        self.assertTrue(is_sorted_embedded_array(arr, key_2d, system_size))
+        self.assertTrue(is_sorted_array_of_nd_positions(arr, key_2d,
+                                                        system_size))
 
     def test_ffft_like_sorting(self):
         # Determine how to order the modes for correct input to the FFFT.
@@ -126,7 +138,7 @@ class ParallelBubbleSortIntegrationTest(unittest.TestCase):
 
         # Find the two rounds of swaps for parallel bubble sort into
         # row-major order.
-        key = partial(index, (0,))
+        key = (index_of_position_in_1d_array, (0,))
         first_round_swaps = parallel_bubble_sort(arr_first, key, system_size)
         second_round_swaps = parallel_bubble_sort(arr_second, key, system_size)
 
@@ -138,7 +150,3 @@ class ParallelBubbleSortIntegrationTest(unittest.TestCase):
         # Compare with the correct full swap network for the FFFT.
         self.assertListEqual(swaps, [1, 3, 5, 2, 4, 1, 3, 5, 3, 2, 4,
                                      1, 3, 5, 0, 2, 4, 6, 1, 3, 5, 2, 4, 3])
-
-
-if __name__ == '__main__':
-    unittest.main()
