aee54719895fdf72eed52139e2bcd67df4a799f4
Jarrod <jarrod.mcc@gmail.com>
2017-08-14 17:47:06 -0700

    Removing lingering references to orbital overlaps (#142)
    * Remove lingering references to orbital overlaps
    
    * Remove additional line from test
    
    * Added tests for molecular data
    
    * Update for Python 2 and 3 compatability on unicode string types for test

--- src/fermilib/utils/_molecular_data.py
+++ src/fermilib/utils/_molecular_data.py
@@ -33,6 +33,12 @@ from fermilib.ops import InteractionOperator, InteractionRDM
     + 0.5 * \sum_{p,q,r,s} h[p,q,r,s] a_p^\dagger a_q^\dagger a_r a_s
 """
 
+# Define a compatible basestring for checking between Python 2 and 3
+try:
+    basestring
+except:
+    basestring = str
+
 
 # Define error objects which inherit from Exception.
 class MoleculeNameError(Exception):
@@ -211,7 +217,6 @@ class MolecularData(object):
         n_qubits: Integer giving total number of qubits that would be needed.
         orbital_energies: Numpy array giving the canonical orbital energies.
         fock_matrix: Numpy array giving the Fock matrix.
-        orbital_overlaps: Numpy array giving the orbital overlap coefficients.
         one_body_integrals: Numpy array of one-electron integrals
         two_body_integrals: Numpy array of two-electron integrals
         mp2_energy: Energy from MP2 perturbation theory.
@@ -271,8 +276,7 @@ class MolecularData(object):
 
         # Metadata fields with default values.
         self.charge = charge
-        if (not isinstance(description, str) and
-                not isinstance(description, unicode)):
+        if (not isinstance(description, basestring)):
             raise TypeError("description must be a string.")
         self.description = description
 
@@ -305,9 +309,6 @@ class MolecularData(object):
         self.hf_energy = None
         self.orbital_energies = None
 
-        # Attributes generated from integrals.
-        self._orbital_overlaps = None
-
         # Attributes generated from MP2 calculation.
         self.mp2_energy = None
 
@@ -328,7 +329,6 @@ class MolecularData(object):
 
         # Molecular orbitals
         self._canonical_orbitals = None
-        self._orbital_overlaps = None
 
         # Electronic Integrals
         self._one_body_integrals = None
@@ -511,12 +511,6 @@ class MolecularData(object):
                                    self.orbital_energies is not None else
                                    False))
             # Save attributes generated from integrals.
-            f.create_dataset("orbital_overlaps",
-                             data=(self.orbital_overlaps if
-                                   self.orbital_overlaps is
-                                   not None else False),
-                             compression=("gzip" if self.orbital_overlaps
-                                          is not None else None))
             f.create_dataset("one_body_integrals",
                              data=(self.one_body_integrals if
                                    self.one_body_integrals is
